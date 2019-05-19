d2042d489431b7cff7fb9d8bc4c5ab592253b7ac
Alicia Tamboli <atamboli4718@gmail.com>
2019-05-18 21:01:58 -0400

    added answer button, fixed timer so it starts after click start button instead of on load

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,43 +8,35 @@
 - on last question, instead of "next question" button, display a "Play again?" button. When clicked start game over
 */
 
-// create a click event for start button
-function startButton() {
-    $("#popUp").text("Start")
-}
-
 //countdown function
 var timeLeft = 30;
-var timer =$("#timer");
-var timerId = setInterval(countdown,1000);
 
 function countdown() {
     if (timeLeft == 0) {
         clearTimeout(timerId)
         alert("You're out of time! The correct answer was...")
-    }
-    else {
+    } else {
         $("#timer").html(timeLeft + " Seconds remaining")
         timeLeft--;
     }
 }
 
-//calls start button function
-startButton ();
+// create a click event for start button
+function startButton() {
+    $("#popUp").text("Start")
+}
 
-$("#popUp").on("click",function (event){
+//calls start button function
+startButton();
+
+$("#popUp").on("click", function (event) {
     $("#popUp").remove();
-    countdown();
+    //countdown(); not needed because countdown function called below
+    var timerId = setInterval(countdown, 1000);
     $(".questionLine").text("What city in the United States is nicknamed Brew City?");
-    $(".answerLine").html("answer1")
+    $("#answer1").html("St. Louis")
+    $("#answer2").html("Houston")
+    $("#answer3").html("Milwaukee")
+    $("#answer4").html("San Jose")
 
 })
-
-
-
-
-
-
-
-
-
